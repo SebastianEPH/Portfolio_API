59e626959899e19ea32c1b8e642d823bcd7e74de
SebastianEPH <sebastianeph99@gmail.com>
2022-04-03 20:38:03 -0500

    fixed names in searching

--- src/controller/controller_person.js
+++ src/controller/controller_person.js
@@ -162,69 +162,70 @@ person.getProjectOnly = async(req, res)=>{
         WHERE project.id = ? 
         `,[project_id])
 
-        let tools  = [];
-        let language  = [];
-        let features = [];
-        let screenshot = [];
-        const pTools = await pool.query(`
-                SELECT
-                    programming_tools.tools,
-                    programming_tools.icon
-                FROM project_tools
-                JOIN programming_tools ON project_tools.tools = programming_tools.id
-                WHERE project_id = ?
-            `,[project_id])
-        const pPerson = await pool.query(`
-                SELECT
-                    *
-                FROM person
-            `)
+    let tools  = [];
+    let language  = [];
+    let features = [];
+    let screenshot = [];
+    const pTools = await pool.query(`
+            SELECT
+                programming_tools.tools,
+                programming_tools.icon
+            FROM project_tools
+            JOIN programming_tools ON project_tools.tools = programming_tools.id
+            WHERE project_id = ?
+        `,[project_id])
+    const pPerson = await pool.query(`
+            SELECT
+                *
+            FROM person
+        `)
 
-        const pLanguage = await pool.query(`
-                SELECT
-                   programming_language.language,
-                   programming_language.icon
-                FROM project_language
-                JOIN programming_language ON project_language.language = programming_language.id
-                WHERE project_id = ?
-            `,[project_id])//   programming_language.lcon
+    const pLanguage = await pool.query(`
+            SELECT
+               programming_language.language,
+               programming_language.icon
+            FROM project_language
+            JOIN programming_language ON project_language.language = programming_language.id
+            WHERE project_id = ?
+        `,[project_id])//   programming_language.lcon
 
-        const pFeatures = await pool.query(`
-                SELECT
-                   *
-                FROM project_feature
-                WHERE project_id = ?
-            `,[project_id])
-        const pScreenshot = await pool.query(`
-                SELECT
-                   *
-                FROM project_screenshot
-                WHERE project_id = ? 
-       
-            `,[project_id])
+    const pFeatures = await pool.query(`
+            SELECT
+               *
+            FROM project_feature
+            WHERE project_id = ?
+        `,[project_id])
+    const pScreenshot = await pool.query(`
+            SELECT
+               *
+            FROM project_screenshot
+            WHERE project_id = ? 
+    
+        `,[project_id])
+    console.log("screenshot pessss *-*****************************", screenshot)
 
-        pScreenshot.map(data=> screenshot.push({
-            screenshot: data.screenshot,
-            details: data.details,
-            number: data.number
-        }));
-        pFeatures.map(data=> features.push({
-            feature: data.feature,
-            description: data.description
-        }));
-        pTools.map(data=> tools.push({
-            tools: data.tools,
-            icon: data.icon
-        }));
-        pLanguage.map(data=> language.push({
-            language:data.language,
-            icon:data.icon
-        }))
-        project[0].person = pPerson[0];
-        project[0].tools = tools;
-        project[0].feature = features;
-        project[0].screenshot = screenshot;
-        project[0].language = language;
+    pScreenshot.map(data=> screenshot.push({
+        screenshot: data.screenshot,
+        details: data.details,
+        number: data.number
+    }));
+    pFeatures.map(data=> features.push({
+        feature: data.feature,
+        description: data.description
+    }));
+    pTools.map(data=> tools.push({
+        tools: data.tools,
+        icon: data.icon
+    }));
+    pLanguage.map(data=> language.push({
+        language:data.language,
+        icon:data.icon
+    }))
+    project[0].person = pPerson[0];
+    project[0].tools = tools;
+    project[0].feature = features;
+    project[0].screenshot = screenshot;
+    project[0].language = language;
 
     res.json(project[0])
 
