26dbf2e7ddbc0fc6d47d0610e017520f8427dfcd
SebastianEPH <sebastianeph99@gmail.com>
2022-04-06 23:43:54 -0500

    finish screenshot

--- src/controller/controller_project.js
+++ src/controller/controller_project.js
@@ -2,83 +2,6 @@ const {parse, DB} = require("../helpers/helpers");
 const pool = require("../database/database");
 const project = {}
 
-project.updateFeature = async(req, res)=>{
-    const {project_id} = req.params
-    //parse IDs from params
-    const parseIds = parse.IdForDB([project_id, req.body.id])
-    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
-
-    // chack if the object data matches
-    const parseBody= parse.ObjDB({...req.body},["feature", "description", "img"], [], [])
-    if(!parseBody.passed){return res.status(parseBody.status).json({message:parseBody.message})}
-
-    try{ // try connection
-        const query = await pool.query(`
-             UPDATE 
-             project_feature 
-             set ?  
-             WHERE id= ? 
-             AND project_id = ? 
-   
-        `,[parseBody.data, req.body.id, project_id])
-        const response =  DB.responseUpd(query)
-        console.log("query maquillado ",response)
-        console.log("query ",query)
-        return res.status(response.status).json({message:response.message})
-    }catch (E){
-        console.log(E)
-        return res.status(400).json({message:"Error fatal, No se pudo procesar la consulta"})
-    }
-
-}
-project.AddFeature = async(req, res)=>{
-    const {project_id} = req.params
-    //parse IDs from params
-    const parseIds = parse.IdForDB([project_id])
-    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
-
-    // chack if the object data matches
-    const parseBody= parse.ObjDB({...req.body,  project_id},["feature", "description", "img"], [], ["project_id"])
-    if(!parseBody.passed){return res.status(parseBody.status).json({message:parseBody.message})}
-
-    console.log("esto es el parse ",parseBody)
-    try{ // try connection
-        const query = await pool.query(`
-             INSERT INTO 
-             project_feature 
-             set ? 
-        `,[parseBody.data])
-
-        const response =  DB.responseAdd(query)
-        console.log(response)
-        return res.status(response.status).json({message:response.message})
-    }catch (E){
-        console.log(E)
-        return res.status(400).json({message:"The submitted data cannot be processed"})
-    }
-}
-
-project.deleteFeature = async(req, res)=>{
-    const {project_id, feature_id} = req.params
-    //parse IDs from params
-    const parseIds = parse.IdForDB([project_id, feature_id])
-    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
-
-    try{ // try connection
-        console.log("las params son", project_id, feature_id)
-        const query = await pool.query(`
-            DELETE FROM project_feature
-            WHERE project_id = ? 
-            AND 
-            id = ?
-        `,[project_id,feature_id])
-        console.log("delete ",query)
-        const response =  DB.responseDel(query)
-        return res.status(response.status).json({message:response.message})
-    }catch (E){
-        return res.status(400).json({message:"Hubo un error al procesar los datos"})
-    }
-}
 
 
 project.getProjectOnly = async(req, res)=>{
@@ -154,6 +77,7 @@ project.getProjectOnly = async(req, res)=>{
                *
             FROM project_screenshot
             WHERE project_id = ? 
+            ORDER BY number ASC;
     
         `,[project_id])
     console.log("screenshot pessss *-*****************************", screenshot)
@@ -189,6 +113,165 @@ project.getProjectOnly = async(req, res)=>{
     res.json(project[0])
 
 
+}
+
+project.AddFeature= async(req, res)=>{
+    const {project_id} = req.params
+    //parse IDs from params
+    const parseIds = parse.IdForDB([project_id])
+    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
+
+    // chack if the object data matches
+    const parseBody= parse.ObjDB({...req.body,  project_id},["feature", "description", "img"], [], ["project_id"])
+    if(!parseBody.passed){return res.status(parseBody.status).json({message:parseBody.message})}
+
+    console.log("esto es el parse ",parseBody)
+    try{ // try connection
+        const query = await pool.query(`
+             INSERT INTO 
+             project_feature 
+             set ? 
+        `,[parseBody.data])
+
+        const response =  DB.responseAdd(query)
+        console.log(response)
+        return res.status(response.status).json({message:response.message})
+    }catch (E){
+        console.log(E)
+        return res.status(400).json({message:"The submitted data cannot be processed"})
+    }
+}
+
+project.updateFeature = async(req, res)=>{
+    const {project_id} = req.params
+    //parse IDs from params
+    const parseIds = parse.IdForDB([project_id, req.body.id])
+    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
+
+    // chack if the object data matches
+    const parseBody= parse.ObjDB({...req.body},["feature", "description", "img"], [], [])
+    if(!parseBody.passed){return res.status(parseBody.status).json({message:parseBody.message})}
+
+    try{ // try connection
+        const query = await pool.query(`
+             UPDATE 
+             project_feature 
+             set ?  
+             WHERE id= ? 
+             AND project_id = ? 
+   
+        `,[parseBody.data, req.body.id, project_id])
+        const response =  DB.responseUpd(query)
+        console.log("query maquillado ",response)
+        console.log("query ",query)
+        return res.status(response.status).json({message:response.message})
+    }catch (E){
+        console.log(E)
+        return res.status(400).json({message:"Error fatal, No se pudo procesar la consulta"})
+    }
+
+}
+
+project.deleteFeature = async(req, res)=>{
+    const {project_id, feature_id} = req.params
+    //parse IDs from params
+    const parseIds = parse.IdForDB([project_id, feature_id])
+    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
+
+    try{ // try connection
+        console.log("las params son", project_id, feature_id)
+        const query = await pool.query(`
+            DELETE FROM project_feature
+            WHERE project_id = ? 
+            AND 
+            id = ?
+        `,[project_id,feature_id])
+        console.log("delete ",query)
+        const response =  DB.responseDel(query)
+        return res.status(response.status).json({message:response.message})
+    }catch (E){
+        return res.status(400).json({message:"Hubo un error al procesar los datos"})
+    }
+}
+
+
+project.AddScreenshot = async(req, res)=>{
+    const {project_id} = req.params
+    //parse IDs from params
+    const parseIds = parse.IdForDB([project_id])
+    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
+
+    // chack if the object data matches
+    const parseBody= parse.ObjDB({...req.body,  project_id},["screenshot", "details", "number"], [], ["project_id"])
+    if(!parseBody.passed){return res.status(parseBody.status).json({message:parseBody.message})}
+
+    console.log("esto es el parse ",parseBody)
+    try{ // try connection
+        const query = await pool.query(`
+             INSERT INTO 
+             project_screenshot
+             set ? 
+        `,[parseBody.data])
+
+        const response =  DB.responseAdd(query)
+        console.log(response)
+        return res.status(response.status).json({message:response.message})
+    }catch (E){
+        console.log(E)
+        return res.status(400).json({message:"The submitted data cannot be processed"})
+    }
+}
+
+project.updateScreenshot = async(req, res)=>{
+    const {project_id} = req.params
+    //parse IDs from params
+    const parseIds = parse.IdForDB([project_id, req.body.id])
+    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
+
+    // chack if the object data matches
+    const parseBody= parse.ObjDB(req.body,["screenshot", "details", "number"], [], ["id"])
+    if(!parseBody.passed){return res.status(parseBody.status).json({message:parseBody.message})}
+
+    try{ // try connection
+        const query = await pool.query(`
+             UPDATE 
+             project_screenshot
+             set ?  
+             WHERE id= ? 
+             AND project_id = ? 
+   
+        `,[parseBody.data, req.body.id, project_id])
+        const response =  DB.responseUpd(query)
+        // console.log("query maquillado ",response)
+        console.log("query ",query)
+        return res.status(response.status).json({message:response.message})
+    }catch (E){
+        console.log(E)
+        return res.status(400).json({message:"Error fatal, No se pudo procesar la consulta"})
+    }
+
+}
+
+project.deleteScreenshot = async(req, res)=>{
+    const {project_id, screenshot_id} = req.params
+    //parse IDs from params
+    const parseIds = parse.IdForDB([project_id, screenshot_id])
+    if(!parseIds.passed){return res.status(parseIds.status).json({message:parseIds.message})}
+
+    try{ // try connection
+        console.log("las params son", project_id, screenshot_id)
+        const query = await pool.query(`
+            DELETE FROM project_screenshot
+            WHERE project_id = ? 
+            AND 
+            id = ?
+        `,[project_id,screenshot_id])
+        console.log("delete ",query)
+        const response =  DB.responseDel(query)
+        return res.status(response.status).json({message:response.message})
+    }catch (E){
+        return res.status(400).json({message:"Hubo un error al procesar los datos"})
+    }
 }
 
 
--- src/routes/project.js
+++ src/routes/project.js
@@ -2,17 +2,19 @@ const express = require('express');
 const routes = express.Router();
 
 const project = require("../controller/controller_project")
-// const accessToken = require('../middleware/index')
-// const {verifyToken} = require ("../middleware/index")
-
 // data
 routes.get('/my/project/:project_id', project.getProjectOnly)
 
+// Feature
 routes.post('/my/project/:project_id/feature', project.AddFeature)
 routes.put('/my/project/:project_id/feature', project.updateFeature)
 routes.delete('/my/project/:project_id/feature/:feature_id', project.deleteFeature)
-// routes.post('/myuser', accessToken.verifyToken, (req, res)=>{
 
-// })
+// Screenshot
+routes.post('/my/project/:project_id/screenshot', project.AddScreenshot)
+routes.put('/my/project/:project_id/screenshot', project.updateScreenshot)
+routes.delete('/my/project/:project_id/screenshot/:screenshot_id', project.deleteScreenshot)
+
+
 
 module.exports = routes;
